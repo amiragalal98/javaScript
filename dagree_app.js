8a4e8d00e5d951519eb49af8b1f954f9cd01bdb2
amiragalal98 <116448908+amiragalal98@users.noreply.github.com>
2023-04-05 22:55:54 +0200

    Create dagree_app.js

---
+++ dagree_app.js
@@ -0,0 +1,9 @@
+// const variable kelvin
+const kelvin = 293;
+//convert kevin to celsius
+const celsius = kelvin - 273 ;
+//convert celsius to fahrenheit
+let fahrenheit = Math.floor(celsius * (9/5) + 32);
+let newton = Math.floor(celsius * (33/100));
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+console.log(`The temperature is ${newton} degrees Newton.`);
